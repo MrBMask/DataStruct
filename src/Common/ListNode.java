f5eddae146ad01b7c38ea2efe0625e880bd931da
郝靓(10029029) <haol-a@glodon.com>
2020-06-28 17:34:16 +0800

    新建ListNode

---
+++ src/Common/ListNode.java
@@ -0,0 +1,16 @@
+package Common;
+
+public class ListNode {
+
+    Node head = null; //头节点
+
+    class Node{
+        Node next;
+        int val;
+        public Node(int val){
+            this.val = val;
+        }
+    }
+
+
+}
